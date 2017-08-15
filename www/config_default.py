efb9c479bb66eae6c32f7046c1a58e0cc8eeeac8
sontailun <taransong@gmail.com>
2017-08-14 23:08:10 -0400

    Up to register and login

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+@author: songtailun
+
+"""
+
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'root',
+        'database': 'python_web'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
